2475041379ead150ef48a657b3343076806c8bad
jeehwan <tjtojan@naver.com>
2024-01-28 13:59:29 +0900

    FEAT: User Entity 생성

---
+++ src/user/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  createDate: Date = new Date();
+
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  updatedDate: Date = new Date();
+
+  @Column()
+  status: string;
+
+  @Column({ default: 0 })
+  failCount: number;
+}
