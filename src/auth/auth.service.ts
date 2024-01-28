5e6a55341cefc5b939916dad16b1a7c09574e869
jeehwan <tjtojan@naver.com>
2024-01-28 17:22:33 +0900

    FEAT: 회원가입 Service 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from 'src/user/user.entity';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
+
+  async register(user: User) {
+    const existedUser = await this.userService.getUser(user.email);
+
+    if (existedUser) {
+      throw new HttpException(
+        '이미 존재하는 이메일입니다.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const encryptedPassword = bcrypt.hashSync(user.password, 10);
+
+    try {
+      const newUser = await this.userService.createUser({
+        ...user,
+        password: encryptedPassword,
+      });
+      newUser.password = undefined;
+      return newUser;
+    } catch (error) {
+      throw new HttpException('Internal Server Error', 500);
+    }
+  }
 }
