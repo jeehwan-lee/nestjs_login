ee93267510156ec09148203b9244846fe6610c4d
jeehwan <tjtojan@naver.com>
2024-01-28 23:23:29 +0900

    FIX: module 오류 수정

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TokenController } from './token.controller';
 import { TokenService } from './token.service';
 import { Token } from './token.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,7 +6,7 @@ import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Token]), JwtModule.register({})],
-  controllers: [TokenController],
+  controllers: [],
   providers: [TokenService],
   exports: [TokenService],
 })
