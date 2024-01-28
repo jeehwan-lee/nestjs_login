5a6cabca5c3d409cf2c9650a6708082c7ac5a467
jeehwan <tjtojan@naver.com>
2024-01-28 14:05:42 +0900

    FEAT: User Controller 생성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('/create')
+  createUser(@Body() user: User) {
+    return this.userService.createUser(user);
+  }
+}
